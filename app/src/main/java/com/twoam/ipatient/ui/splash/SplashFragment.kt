64e7a12371a952ce134a9afb083641addb4a44c9
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-02-20 21:08:56 +0200

    home - create menu splash - navigate to home or login according to credentials

--- app/src/main/java/com/twoam/ipatient/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/twoam/ipatient/ui/splash/SplashFragment.kt
@@ -1,24 +1,58 @@
 package com.twoam.ipatient.ui.splash
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.navigation.NavigationView
 import com.twoam.ipatient.R
+import com.twoam.ipatient.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
+    //region Variables
+    private lateinit var binding: FragmentSplashBinding
+     private val duration=1500L
+    //endregion
 
+    //region Events
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigate()
+    }
 
+    //endregion Helper FUnctions
+    private fun navigate() {
+
+        object : CountDownTimer(duration, 1000) {
+            override fun onTick(p0: Long) {
+            }
+
+            override fun onFinish() {
+                if (true) {
+                    findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+                } else {
+                    findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+                }
+            }
+        }
+
+    }
+
+    //region
+    companion object {
+        private const val TAG = "SplashFragment"
     }
 }
