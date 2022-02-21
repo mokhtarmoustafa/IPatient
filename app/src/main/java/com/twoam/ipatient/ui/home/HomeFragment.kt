8ceb3724d63f2e5575738a561455da73b84e66e0
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-02-21 09:13:15 +0200

    app - adjust pallet colors

--- app/src/main/java/com/twoam/ipatient/ui/home/HomeFragment.kt
+++ app/src/main/java/com/twoam/ipatient/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.twoam.ipatient.ui.home
 
+import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,22 +8,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.twoam.ipatient.R
+import com.twoam.ipatient.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
+
+    //region variables
+
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
+    //endregion
 
     companion object {
         fun newInstance() = HomeFragment()
     }
 
-    private lateinit var viewModel: HomeViewModel
 
+    //region events
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        return inflater.inflate(R.layout.home_fragment, container, false)
+    ): View {
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showEmptyView(true)
+    }
+
+
+    //endregion
+
+    //region helper functions
+
+    private fun showEmptyView(isEmpty: Boolean) {
+        if (isEmpty) binding.groupEmptyData.visibility = View.VISIBLE else View.INVISIBLE
+    }
+    //endregion
 
 }
